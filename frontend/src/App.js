81179810b16e8884f61cb30ecbc285ed14d39e25
abhimran <abhimrann@gmail.com>
2021-06-12 13:54:01 +0600

    create product list component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Footer from './component/Footer';
 import Header from './component/Header';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
       <main className={`py-3`}>
-        <Container>Welcome to proshop!</Container>
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
